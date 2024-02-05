42415b1d1f5e7f0a0fdddb86c372eb789f5492f7
jtagcat <git-12dbd862@jtag.cat>
2024-02-06 01:51:15 +0200

    /mikroskeem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use async_trait::async_trait;
 use frankenstein::{
     AsyncApi, AsyncTelegramApi, GetUpdatesParams, Message, ReplyParameters, SendMessageParams,
     UpdateContent,
@@ -26,12 +27,24 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
             Ok(response) => {
                 for update in response.result {
-                    if let UpdateContent::Message(message) = update.content {
-                        let api_clone = api.clone();
+                    match update.content {
+                        UpdateContent::Message(message) => {
+                            let api = api.clone();
 
-                        tokio::spawn(async move {
-                            _ = reqex_message(message, api_clone).await;
-                        });
+                            tokio::spawn(async move {
+                                let handlers: &[&dyn MessageHandler] = &[&Shrug, &Reqex];
+
+                                for handler in handlers {
+                                    let handler = handler;
+                                    if handler.test(&message).await {
+                                        _ = handler.handle(message, api).await;
+                                        return
+                                    }
+                                }
+                            });
+                        }
+
+                        _ => {}
                     }
 
                     update_params = update_params_builder
@@ -44,43 +57,75 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
 }
 
+#[async_trait]
+trait MessageHandler {
+    async fn test(&self, message: &Message) -> bool;
+    async fn handle(&self, message: Message, api: AsyncApi) -> Option<()>;
+}
+
+// fn message_handlers() ->  {
+//
+// }
+
+struct Shrug;
+#[async_trait]
+impl MessageHandler for Shrug {
+    async fn test(&self, message: &Message) -> bool {
+        message.text.as_ref().is_some_and(|t| t == "/shrug")
+    }
+
+    async fn handle(&self, message: Message, api: AsyncApi) -> Option<()> {
+        let result_message = SendMessageParams::builder()
+            .chat_id(message.chat.id)
+            .text("¯\\_(ツ)_/¯")
+            .build();
+
+        if let Err(err) = api.send_message(&result_message).await {
+            println!("Failed to send shrug: {err:?}");
+            return None;
+        }
+
+        Some(())
+    }
+}
+
 lazy_static! {
     static ref SED_EXPR_RE: Regex = Regex::new("^s/(.*)/(.*)/.*").expect("Failed compiling regex");
 }
 
-async fn reqex_message(message: Message, api: AsyncApi) -> Option<()> {
-    // validate input
-    let user_pattern = message.text?;
-    let message_replyof = *(message.reply_to_message?);
-    let user_input = message_replyof.text?;
-
-    if !SED_EXPR_RE.is_match(&user_pattern) {
-        return None;
-    };
-
-    println!(
-        "<{}.{:?}>: {}",
-        message.chat.id, message.from?.first_name, user_pattern
-    );
-
-    let Ok(result) = find_and_replace(&user_input, &[user_pattern]) else {
-        return None;
-    };
-
-    let result_message = SendMessageParams::builder()
-        .chat_id(message.chat.id)
-        .text(result)
-        .reply_parameters(
-            ReplyParameters::builder()
-                .message_id(message.message_id)
-                .build(),
-        )
-        .build();
-
-    if let Err(err) = api.send_message(&result_message).await {
-        println!("Failed to send reply: {err:?}");
-        return None;
+struct Reqex;
+#[async_trait]
+impl MessageHandler for Reqex {
+    async fn test(&self, message: &Message) -> bool {
+        message
+            .text
+            .as_ref()
+            .is_some_and(|user_pattern| SED_EXPR_RE.is_match(&user_pattern))
     }
 
-    Some(())
+    async fn handle(&self, message: Message, api: AsyncApi) -> Option<()> {
+        let user_input = message.reply_to_message?.text?;
+        let user_pattern = message.text?;
+
+        let Ok(result) = find_and_replace(&user_input, &[user_pattern]) else {
+            return None;
+        };
+
+        let result_message = SendMessageParams::builder()
+            .chat_id(message.chat.id)
+            .text(result)
+            .reply_parameters(
+                ReplyParameters::builder()
+                    .message_id(message.message_id)
+                    .build(),
+            )
+            .build();
+
+        if let Err(err) = api.send_message(&result_message).await {
+            println!("Failed to send reply: {err:?}");
+            return None;
+        }
+
+        Some(())
+    }
 }
